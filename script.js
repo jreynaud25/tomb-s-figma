d014b4e3888e372074be32059d13d2477750e89e
Jean Reynaud <jreynaud@MacBook-Pro-de-Jean.local>
2022-02-11 15:16:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log("tests");
+// const myTimeout = setTimeout(myGreeting, 8000);
+
+// function myGreeting() {
+//   $("iframe").each(function (index) {
+//     $(this)
+//       .contents()
+//       .find(".embed_documentation_footer--documentationFooter--2gpG4")
+//       .css("display", "none");
+//   });
+
+//   var iframe = document.getElementById("myFrame");
+//   var elmnt = iframe.contentWindow.document.getElementsByTagName("div")[0];
+//   elmnt.style.display = "none";
+//   console.log("DONE");
+// }
