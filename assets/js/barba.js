f842839e021f42d477df41c53c8bd20103a762ea
Jean Reynaud <jreynaud@MacBook-Pro-de-Jean.local>
2022-02-13 23:42:28 +0100

    ok

---
+++ assets/js/barba.js
@@ -0,0 +1,125 @@
+barba.init({
+  transitions: [{
+    name: 'custom-transition',
+    from: {
+      namespace: [
+        'home'
+      ]
+    },
+    to: {
+      namespace: [
+        'tombs'
+      ]
+    },
+    leave(){
+      gsap.set(".text-container .text-col:nth-child(1)", { background: 'black'});
+      gsap.to(".text-container .text-col:nth-child(2)", { ease: Power1.easeOut, xPercent: -105, duration: .2});
+      gsap.to(".text-container .text-col:nth-child(3)", { ease: Power1.easeOut, xPercent: -210, duration: .4});
+      gsap.fromTo(".tombs-wrapper", {xPercent: 41, opacity: 0.5},{ ease: Power1.easeOut, xPercent: 0, opacity: 1, duration: .4});
+
+      const done = this.async();
+      setTimeout(() => {
+        done()
+      }, 800);
+    },
+    enter(){
+      gsap.set(".text-container .text-col:nth-child(2)", { xPercent: -105});
+      gsap.set(".text-container .text-col:nth-child(3)", { xPercent: -210});
+      gsap.set(".text-container .text-col:nth-child(1)", { background: 'black'});
+
+    }
+  },{
+    name: 't-t-h',
+    from: {
+      namespace: [
+        'tombs'
+      ]
+    },
+    to: {
+      namespace: [
+        'home'
+      ]
+    },
+    leave(){
+      gsap.set(".text-container .text-col:nth-child(1)", { background: 'black'});
+      gsap.set(".text-container .text-col:nth-child(2)", { opacity: 1});
+      gsap.set(".text-container .text-col:nth-child(3)", { opacity: 1});
+      gsap.fromTo(".text-container .text-col:nth-child(2)", { xPercent: -105},  { ease: Power1.easeOut, xPercent: 0, delay: .2 ,duration: .2});
+      gsap.fromTo(".text-container .text-col:nth-child(3)", { xPercent: -210}, { ease: Power1.easeOut, xPercent: 0, duration: .4});
+      gsap.to(".tombs-wrapper", { ease: Power1.easeOut, xPercent: 41, opacity: 0.5, duration: .4});
+
+      const done = this.async();
+      setTimeout(() => {
+        done()
+      }, 600);
+    },
+    enter(){
+      gsap.set(".tombs-wrapper", {xPercent: 41});
+
+    }
+  },
+  {
+    name: 't-t-h',
+    from: {
+      namespace: [
+        'tombs',
+        'home'
+      ]
+    },
+    to: {
+      namespace: [
+        'tomb-index'
+      ]
+    },
+    leave(){
+      gsap.to(".text-container", { ease: Power1.easeOut, xPercent: -100, opacity: 0.5, duration: .6});
+      gsap.to(".tombs-wrapper", { ease: Power1.easeOut, xPercent: 100, opacity: 0.5, duration: .6});
+
+      const done = this.async();
+      setTimeout(() => {
+        done()
+      }, 600);
+    },
+    enter(){
+      gsap.fromTo(".text-container", {opacity: 0},{ ease: Power1.easeOut,  opacity: 1, duration: .8});
+      gsap.fromTo(".tombs-wrapper", {yPercent: 100},{ yPercent: 0, ease: Power1.easeOut,  opacity: 1, duration: .4});
+
+    }
+  },
+  {
+    name: 't-t-h',
+    from: {
+      namespace: [
+        'tomb-index'
+      ]
+    },
+    to: {
+      namespace: [
+        'tombs',
+        'home'
+      ]
+    },
+    leave(){
+      gsap.fromTo(".text-container", {opacity: 1},{ ease: Power1.easeOut,  opacity: 0, duration: .8});
+      gsap.fromTo(".tombs-wrapper", {yPercent: 0},{ yPercent: 100, ease: Power1.easeOut,  opacity: 1, duration: .4});
+      const done = this.async();
+      setTimeout(() => {
+        done()
+      }, 600);
+    },
+    enter(data){
+      gsap.fromTo(".text-container", { xPercent: -100}, { ease: Power1.easeOut, xPercent: 0, opacity: 1, duration: .6});
+      gsap.fromTo(".tombs-wrapper",  { xPercent: 100}, { ease: Power1.easeOut, xPercent: 0, opacity: 1, duration: .6});
+      if(data.next.namespace == "home"){
+        gsap.fromTo(".tombs-wrapper",  { xPercent: 100}, { ease: Power1.easeOut, xPercent: 41, opacity: 1, duration: .6});
+      }
+      else {
+        gsap.fromTo(".tombs-wrapper",  { xPercent: 100}, { ease: Power1.easeOut, xPercent: 0, opacity: 1, duration: .6});
+      }
+      if(data.next.namespace == "tombs"){
+        gsap.set(".text-container .text-col:nth-child(2)", { xPercent: -105, opacity: 0});
+        gsap.set(".text-container .text-col:nth-child(3)", { xPercent: -210, opacity: 0});
+      }
+    }
+  }]
+});
---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+function createCarousel(){
+var elem = document.querySelector('.tombs-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  controls: false,
+  prevNextButtons: false,
+  pageDots: false
+
+
+});
+
+flkty.on( 'settle', function( index ) {
+  console.log('ok')
+  var number = document.querySelector('.is-selected').dataset.number
+  var name = document.querySelector('.is-selected').dataset.name
+  var spans = document.querySelectorAll('.tomb-title-container span');
+  spans[0].innerHTML = number
+  spans[1].innerHTML = name
+
+});}
+createCarousel()
+
+
+
+// alert('ok')
+
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity( '.tombs-carousel', {
+//   // options
+// });
+
+var end = new Date('18 Feb 2022 15:00:00');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour *24;
+var timer;
+
+function showRemaining()
+{
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0 ) {
+       // handle expiry here..
+       clearInterval( timer ); // stop the timer from continuing ..
+       //alert('Expired'); // alert a message that the timer has expired..
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor( (distance % _day ) / _hour );
+    var minutes = Math.floor( (distance % _hour) / _minute );
+    var seconds = Math.floor( (distance % _minute) / _second );
+    var milliseconds = distance % _second;
+
+    var countdownElement = document.getElementById('timer');
+    countdownElement.innerHTML = days  + 'd ' +
+                                 hours + 'h ' +
+                                 minutes + 'm ' +
+                                 seconds + 's '}
+
+timer = setInterval(showRemaining, 10);
